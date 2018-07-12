0fc6fc692378fb2468d90870374b3ccc0df09fcc
Tartaglia <erik.gui.tartaglia@dxc.com>
2018-07-12 16:31:09 -0300

    [change]: Cleanup

--- app/src/main/java/br/com/tartagliaeg/rxp/debug/TestActivity.java
+++ app/src/main/java/br/com/tartagliaeg/rxp/debug/TestActivity.java
@@ -29,7 +29,7 @@ public class TestActivity extends LogActivity {
   @Override
   public void onCreate(Bundle savedInstanceState) {
     super.onCreate(savedInstanceState);
-    setContentView(R.layout.activity_main);
+    setContentView(R.layout.activity_test);
     Toolbar toolbar = findViewById(R.id.toolbar);
     setSupportActionBar(toolbar);
 
